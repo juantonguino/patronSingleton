7e968fb379c14c76e4bb04d385e198d2f42513ce
juantonguino <juan.tonguino@gmail.com>
2015-03-20 10:45:25 -0500

    subiendo archivos

---
+++ patronSingleton/clases/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace patronSingleton.clases
+{
+    
+    class Cliente
+    {
+        public Configurador con;
+        
+        public Cliente()
+        {
+            con = Configurador.darInstancia("","");
+        }
+
+        public String darMensaje()
+        {
+            return Configurador.darMensaje();
+        }
+
+    }
+}
---
+++ patronSingleton/clases/Configurador.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace patronSingleton.clases
+{
+    public class Configurador
+    {
+        private String url;
+        private String baseDatos;
+        private static Configurador instancia;
+        private static String mensaje;
+
+        private Configurador(String url, String baseDatos)
+        {
+            this.url = url;
+            this.baseDatos = baseDatos;
+        }
+        
+        public static Configurador darInstancia(String url, String baseDatos){
+            if (instancia == null)
+            {
+                instancia = new Configurador(url, baseDatos);
+                mensaje = "es la primera vez ques se crea este configurador";
+                return instancia;
+            }
+            else
+            {
+                mensaje = "este objeto ya ha sido instanciado";
+                return instancia;
+            }
+        }
+        public static String darMensaje()
+        {
+            return mensaje;
+        }
+    }
+}
